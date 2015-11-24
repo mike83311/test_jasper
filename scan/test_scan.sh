9abdab63aa367e1bdac7b0bc831d1166ca0c6c10
mike83311 <mike83311@gmail.com>
2015-11-24 15:24:28 +0800

    Update test_scan.sh

--- scan/test_scan.sh
+++ scan/test_scan.sh
@@ -2,41 +2,34 @@
 
 sudo iwlist wlan0 scan > wifi_result.txt
 
-
-dbHCL404=$(grep -i HCL-404 -A 16 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
-if [ -z "$dbHCL404" ]
-then
-	dbHCL404=0
-fi
-
-dbE403=$(grep -i E403 -A 20 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
-if [ -z "$dbE403" ]
-then
-	dbE403=0
-fi
-
-dbE406=$(grep -i E406 -A 16 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
-if [ -z "$dbE406" ]
-then
-	dbE406=0
-fi
-
-#dbRandy=$(grep -i Randy-WiFi -A 16 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
-#if [ -z "$dbRandy" ]
-#then
-#	dbRandy=0
-#fi
-
-dbICL3=$(grep -i ICL3 -A 16 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
+dbICL3=$(grep -i B8:55:10:2E:65:70 -A 12 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
 if [ -z "$dbICL3" ]
 then
 	dbICL3=0
 fi
 
-dbLab411=$(grep -i Lab411 -A 20 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
-if [ -z "$dbLab411" ]
+dbE403=$(grep -i CC:B2:55:5D:3B:66 -A 18 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
+if [ -z "$dbE403" ]
 then
-        dbLab411=0
+	dbE403=0
+fi
+
+dbHCL404=$(grep -i 30:B5:C2:BD:DA:C1 -A 12 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
+if [ -z "$dbHCL404" ]
+then
+	dbHCL404=0
+fi
+
+dbE406=$(grep -i C0:A0:BB:14:56:8C -A 17 wifi_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
+if [ -z "$dbE406" ]
+then
+	dbE406=0
+fi
+
+dbE407=$(grep -i 9C:D6:43:D6:91:F8 -A 13 scan_result.txt | awk '/level/' |cut -d '=' -f 3 |cut -d '/' -f 1)
+if [ -z "$dbE407" ]
+then
+        dbE407=0
 fi
 
 #echo $dbHCL404 $dbE403 $dbE406 $dbICL3 $dbLab411
@@ -44,13 +37,13 @@ fi
 #echo "Now Position is : "
 
 #echo $position
-position=$(/home/pi/scan/wifi $dbHCL404 $dbE403 $dbE406 $dbICL3 $dbLab411)
+position=$(~/test_jasper/scan/wifi $dbICL3 $dbE403 $dbHCL404 $dbE406 $dbE407)
 posx=$(echo $position | cut -c 1) 
 posy=$(echo $position | cut -c 3)
 #echo $posx
 #echo $posy
 
-/home/pi/scan/Astar 20 3
+~/test_jasper/scan/Astar 20 3
 
 
 
